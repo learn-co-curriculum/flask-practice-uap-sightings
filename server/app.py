b37b6455d5a6ca681906e37a8b410b4cdd1225c4
betalantz <betalantz@gmail.com>
2025-01-08 14:17:24 -0800

    adds routes through 5

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, request
+from flask import Flask, abort, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -16,6 +16,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Initialize Flask-Migrate
 migrate = Migrate(app, db)
 
+api = Api(app)
+
 # Initialize the db instance
 db.init_app(app)
 # Define routes and views
@@ -24,6 +26,33 @@ db.init_app(app)
 @app.route("/")
 def home():
     return make_response("The UAPID welcome our new extraterrestrial overlords!", 200)
+
+
+@app.route("/sightings/location/<string:location>")
+def show(location):
+    s_list = Sighting.query.filter(Sighting.location == location).all()
+    if s_list.length == 0:
+        return make_response("There are no sightings at that location", 404)
+    return make_response([s.to_dict() for s in s_list], 200)
+
+
+class Sightings(Resource):
+    def get(self):
+        sightings = [s.to_dict() for s in Sighting.query.all()]
+        return make_response(sightings, 200)
+
+
+class SightingById(Resource):
+
+    def get(self, id):
+        s = db.session.get(Sighting, id)
+        if not s:
+            abort(404)
+        return make_response(s.to_dict(), 200)
+
+
+api.add_resource(Sightings, "/sightings")
+api.add_resource(SightingById, "/sightings/<int:id>")
 
 
 if __name__ == "__main__":
