c55f311faaccc80204b0019a88eb3cfff6219a48
betalantz <betalantz@gmail.com>
2025-01-08 08:43:11 -0800

    creates models, configs app, home route

--- server/app.py
+++ server/app.py
@@ -2,18 +2,29 @@
 
 from flask import Flask, make_response, request
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 # import model and db instance
+from models import Sighting, db
 
 # Initialize Flask app
+app = Flask(__name__)
 
 # configure the app
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Initialize Flask-Migrate
+migrate = Migrate(app, db)
 
 # Initialize the db instance
-
+db.init_app(app)
 # Define routes and views
+
+
+@app.route("/")
+def home():
+    return make_response("The UAPID welcome our new extraterrestrial overlords!", 200)
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 # Initialize Flask-SQLAlchemy
+db = SQLAlchemy(engine_options={"echo": True})
+
 
 # Define the Sighting model
+class Sighting(db.Model, SerializerMixin):
+    __tablename__ = "sightings"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    time = db.Column(db.String)
+    location = db.Column(db.String)
+    shape_of_craft = db.Column(db.String)
+    approximate_size = db.Column(db.Integer)
+    approximate_speed = db.Column(db.Integer)
+    description = db.Column(db.String)
+    reporter = db.Column(db.String)
+    reporter_reliable_witness = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"<Sighting {self.id} | location: {self.location} | date: {self.date} | reporter: {self.reporter} >"
